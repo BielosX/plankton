da3895b0d16f643ceea99dae5d273532943622d0
Mateusz Biel <matbiel@gmail.com>
2025-01-26 21:21:48 +0100

    Add main func

---
+++ src/Main.fs
@@ -0,0 +1,12 @@
+﻿module Main
+
+open Microsoft.AspNetCore.Builder
+open System
+
+[<EntryPoint>]
+let main args =
+    let app = WebApplication.Create(args)
+    app.UseWebSockets() |> ignore
+    app.MapGet("/health", Func<string>(fun () -> "OK")) |> ignore
+    app.Run()
+    0
